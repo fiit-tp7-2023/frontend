d918407e77397afd8889ef1a6ae71ddcf1817c56
Branislav Hozza <brankohozza@gmail.com>
2023-10-20 09:33:03 +0200

    :sparkles: add fetch example

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h1>Project section index page</h1>
+    <button @click="refreshForecast()">Refresh forecast</button>
+    <div>{{ forecast }}</div>
   </div>
 </template>
+<script setup lang="ts">
+const { data } = useFetch("/api/token");
+const { data: forecast, refresh: refreshForecast } = useAsyncData(
+  "forecast",
+  () =>
+    $fetch("https://localhost:7210/WeatherForecast", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer: " + data.value?.googleAccessToken,
+      },
+    })
+);
+</script>
