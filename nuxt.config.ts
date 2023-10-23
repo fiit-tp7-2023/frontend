cf51f8e3118c7e9776134a6086e406a285e05ac5
Samuel Hetteš <samuel.hettes@gmail.com>
2023-10-23 20:11:47 +0200

    :sparkles: Add NaiveUI and Nuxt Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,16 @@ export default defineNuxtConfig({
     googleClientId: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   },
-  modules: ["@sidebase/nuxt-auth"],
+  modules: ["@sidebase/nuxt-auth", "nuxt-icon"],
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? ["naive-ui", "vueuc", "@css-render/vue3-ssr", "@juggle/resize-observer"]
+        : ["@juggle/resize-observer"],
+  },
+  vite: {
+    optimizeDeps: {
+      include: process.env.NODE_ENV === "development" ? ["naive-ui", "vueuc", "date-fns-tz/formatInTimeZone"] : [],
+    },
+  },
 });
