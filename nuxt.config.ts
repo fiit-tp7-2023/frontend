e24e7b098576fe1e481fea53c5985892a3b1980f
Branislav Hozza <brankohozza@gmail.com>
2023-10-25 21:03:54 +0200

    :wrench: configure auth secret

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   auth: {
     enableGlobalAppMiddleware: true,
     origin: process.env.NODE_ENV === 'production' ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000',
+    secret: process.env.AUTH_SECRET,
   },
   runtimeConfig: {
     googleClientId: process.env.GOOGLE_CLIENT_ID,
