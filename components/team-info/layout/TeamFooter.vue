d7fca13454af6bf6745bcb5581c2f9a26e2e2b79
Christian Danizek <christian@danizek.com>
2023-10-28 13:02:17 +0200

    💄 update footer content

--- components/team-info/layout/TeamFooter.vue
+++ components/team-info/layout/TeamFooter.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Footer</div>
+  <div class="flex place-content-center items-center space-x-3 bg-zinc-800 text-white h-14">
+    <div class="h-6">
+      <NuxtLink to="https://github.com/fiit-tp7-2023" target="_blank">
+        <Icon size="1.5em" name="mdi:github" color="white" class="mr-1" />
+        <p class="inline-block">GitHub</p>
+      </NuxtLink>
+    </div>
+    <div class="h-6">•</div>
+    <div class="h-6">{{ new Date().getFullYear() }}</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
