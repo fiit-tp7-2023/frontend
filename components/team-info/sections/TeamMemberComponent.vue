bcd47ab730f9285b694eaa29eca5ac3df87cf80b
Christian Danizek <christian@danizek.com>
2023-10-29 16:12:49 +0100

    ✨ add team member component and team section component

---
+++ components/team-info/sections/TeamMemberComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="table text-center w-28">
+    <img :src="image" alt="avatar" class="rounded-full overflow-hidden w-24 h-24 m-auto" />
+    <p class="uppercase text-xl">{{ props.name }}</p>
+    <p>{{ props.role }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+    default: null,
+  },
+});
+
+const image = props.image ?? `https://api.dicebear.com/7.x/pixel-art/png?seed=${props.name}`;
+</script>
+
+<style></style>
---
+++ components/team-info/sections/TeamSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col items-center space-y-5">
+    <h1 class="text-3xl font-bold mb-0">Product Owners</h1>
+    <div class="flex space-x-10">
+      <team-member-component name="Viktor" role="Role" />
+      <team-member-component name="Kiko" role="Role" />
+    </div>
+    <h1 class="text-3xl font-bold mb-0">Our Team</h1>
+    <div class="flex space-x-10">
+      <team-member-component name="Adam" role="Role" />
+      <team-member-component name="Karolína" role="Role" />
+      <team-member-component name="Alexandra" role="Role" />
+    </div>
+    <div class="flex space-x-10">
+      <team-member-component name="Ctibor" role="Role" />
+      <team-member-component name="Kiko" role="Role" />
+      <team-member-component name="Braňo" role="Role" />
+      <team-member-component name="Samuel" role="Role" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TeamMemberComponent from './TeamMemberComponent.vue';
+</script>
+
+<style></style>
