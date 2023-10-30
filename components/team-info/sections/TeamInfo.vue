69ef5bfe2b07754cb0cf95c272e38c4d54b857db
Alexandra Guňovská <71826434+xgunovska@users.noreply.github.com>
2023-10-30 15:33:50 +0100

    :recycle: split teaminfo into component

--- components/team-info/sections/TeamInfo.vue
+++ components/team-info/sections/TeamInfo.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <div id="about" class="mx-auto max-w-7xl px-6 lg:px-8 grid lg:grid-cols-2 gap-8 column-2 mb-10">
+    <div class="mx-auto max-w-2xl lg:mx-0 mb-10">
+      <div>
+        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">About our project</h2>
+      </div>
+      <div>
+        <p class="mt-6 text-lg leading-8 text-gray-300">
+          At the heart of our project lies a fascination with the intricate details encoded in blockchain transactions.
+          We've set our sights on collecting and dissecting the metadata of NFTs, treating each piece of information as
+          a puzzle piece that, when put together, forms a captivating narrative. Through meticulous data science
+          techniques, we aim to uncover the relationships between various addresses, shedding light on the
+          interconnected web of transactions within the blockchain.
+        </p>
+        <p class="mt-6 text-lg leading-8 text-gray-400">
+          Armed with our findings, we are channeling our data into the dynamic realm of Neo4j, a powerful graph
+          database. Here, relationships between addresses will materialize into a visual symphony, providing insights
+          into the patterns and connections that define the NFT landscape. Our Neo4j graph database is not just a
+          repository; it's a canvas where the story of blockchain transactions unfolds in vibrant detail.
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-center items-center mt-3 mr-11">
+      <n-button
+        class="border-4 w-1/2 h-20 content-center ml-auto text-white font-medium rounded-lg text-sm text-center"
+        ghost
+        @click="$router.push('/project/login')"
+        >Project section
+      </n-button>
+    </div>
+  </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,11 @@
 <template>
   <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-    <!-- <img
-      src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-      alt=""
-      class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-    /> -->
     <div
       class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
       aria-hidden="true"
     >
       <div
-        class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
+        class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20 tag-bg-gradient"
       />
     </div>
     <div
@@ -38,67 +13,42 @@
       aria-hidden="true"
     >
       <div
-        class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
+        class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20 tag-bg-gradient"
       />
     </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 grid lg:grid-cols-2 gap-8 column-2">
-      <div class="mx-auto max-w-2xl lg:mx-0 mb-10">
-        <div>
-          <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">About our project</h2>
-        </div>
-        <div>
-          <p class="mt-6 text-lg leading-8 text-gray-300">
-            At the heart of our project lies a fascination with the intricate details encoded in blockchain
-            transactions. We've set our sights on collecting and dissecting the metadata of NFTs, treating each piece of
-            information as a puzzle piece that, when put together, forms a captivating narrative. Through meticulous
-            data science techniques, we aim to uncover the relationships between various addresses, shedding light on
-            the interconnected web of transactions within the blockchain.
-          </p>
-          <p class="mt-6 text-lg leading-8 text-gray-400">
-            Armed with our findings, we are channeling our data into the dynamic realm of Neo4j, a powerful graph
-            database. Here, relationships between addresses will materialize into a visual symphony, providing insights
-            into the patterns and connections that define the NFT landscape. Our Neo4j graph database is not just a
-            repository; it's a canvas where the story of blockchain transactions unfolds in vibrant detail.
-          </p>
-        </div>
-      </div>
-      <div class="flex justify-center items-center mt-3 mr-11">
-        <n-button
-          class="content-center ml-auto text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          type="info"
-          @click="$router.push('/project/login')"
-          >Project section</n-button
-        >
-      </div>
-    </div>
-    <team-members />
+    <team-info />
+    <team-section />
   </div>
 </template>
 <script lang="ts" setup>
-import TeamMembers from '../components/team-info/sections/TeamMembers.vue';
+import TeamSection from '../components/team-info/sections/TeamSection.vue';
+import TeamInfo from '~/components/team-info/sections/TeamInfo.vue';
 
 definePageMeta({
   auth: false,
   layout: 'team',
 });
 </script>
+
+<style scoped>
+.tag-bg-gradient {
+  clip-path: polygon(
+    74.1% 44.1%,
+    100% 61.6%,
+    97.5% 26.9%,
+    85.5% 0.1%,
+    80.7% 2%,
+    72.5% 32.5%,
+    60.2% 62.4%,
+    52.4% 68.1%,
+    47.5% 58.3%,
+    45.2% 34.5%,
+    27.5% 76.7%,
+    0.1% 64.9%,
+    17.9% 100%,
+    27.6% 76.8%,
+    76.1% 97.7%,
+    74.1% 44.1%
+  );
+}
+</style>
