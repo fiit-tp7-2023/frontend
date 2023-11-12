e9ef59f8fd02fade1e9298544d71e4feb9c207e8
Karolína Tóthová <56071209+ktothova@users.noreply.github.com>
2023-11-12 23:00:25 +0100

    :lipstick: Changed roles to real ones

--- components/team-info/sections/TeamSection.vue
+++ components/team-info/sections/TeamSection.vue
@@ -2,19 +2,19 @@
   <div id="team" class="overflow-hidden md:flex flex-col items-center space-y-10">
     <h1 class="text-3xl text-center font-bold mb-0">Our Team</h1>
     <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
-      <team-member-component name="Viktor" role="Role" />
-      <team-member-component name="Kiko" role="Role" />
+      <team-member-component name="Viktor" role="Product owner" />
+      <team-member-component name="Kiko" role="Product owner" />
     </div>
     <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
-      <team-member-component name="Adam" role="Role" />
-      <team-member-component name="Karolína" role="Role" />
-      <team-member-component name="Alexandra" role="Role" />
+      <team-member-component name="Adam" role="Indexer" />
+      <team-member-component name="Karolína" role="UX designer" />
+      <team-member-component name="Alexandra" role="UX designer" />
     </div>
     <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
-      <team-member-component name="Ctibor" role="Role" />
-      <team-member-component name="Kiko" role="Role" />
-      <team-member-component name="Braňo" role="Role" />
-      <team-member-component name="Samuel" role="Role" />
+      <team-member-component name="Ctibor" role="Data analyst" />
+      <team-member-component name="Kiko" role="Front-end developer" />
+      <team-member-component name="Braňo" role="Scrum master" />
+      <team-member-component name="Samuel" role="Back-end developer" />
     </div>
   </div>
 </template>
@@ -22,3 +22,4 @@
 <script lang="ts" setup>
 import TeamMemberComponent from './TeamMemberComponent.vue';
 </script>
+
