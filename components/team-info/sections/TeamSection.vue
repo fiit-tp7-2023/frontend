0540aaa0744b19a6599c1f34e76e11bd4f2b979d
Karolína Tóthová <56071209+ktothova@users.noreply.github.com>
2023-11-12 21:30:37 +0100

    :sparkles: added responsiveness to team section

--- components/team-info/sections/TeamSection.vue
+++ components/team-info/sections/TeamSection.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="team" class="overflow-hidden flex flex-col items-center space-y-5">
-    <h1 class="text-3xl font-bold mb-0">Our Team</h1>
-    <div class="flex space-x-10">
+  <div id="team" class="overflow-hidden md:flex flex-col items-center space-y-10">
+    <h1 class="text-3xl text-center font-bold mb-0">Our Team</h1>
+    <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
       <team-member-component name="Viktor" role="Role" />
       <team-member-component name="Kiko" role="Role" />
     </div>
-    <div class="flex space-x-10">
+    <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
       <team-member-component name="Adam" role="Role" />
       <team-member-component name="Karolína" role="Role" />
       <team-member-component name="Alexandra" role="Role" />
     </div>
-    <div class="flex space-x-10">
+    <div class="flex flex-col items-center space-y-10 md:flex-row md:space-y-0 md:space-x-10">
       <team-member-component name="Ctibor" role="Role" />
       <team-member-component name="Kiko" role="Role" />
       <team-member-component name="Braňo" role="Role" />
